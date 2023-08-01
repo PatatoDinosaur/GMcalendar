d7782d67438c21a51445ec7adf6c9d60bd05a89f
PatatoDinosaur <pengirku@gmail.com>
2023-08-01 03:54:38 +0000

    初期設計

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    </head>
+    <x-app-layout>
+        <x-slot name="header">
+            <body class="anticliased">
+                <h1>グループ作成</h1>
+                <br>
+                <form action="/posts" method="POST">
+                    @csrf
+                    <div class ="title">
+                        <h2>グループ名</h2>
+                        <input type="text" name="post[title]" placeholder=""/>
+                    </div>
+                    <div class="body">
+                        <h2>グループ情報</h2>
+                        <textarea name="post[body]" placeholder="みんなでワイワイやってます！"></textarea>
+                    </div>
+                    <div class = category>
+                        <h2>カテゴリー</h2>
+                        <select name="post[category_id]">
+                            @foreach($categories as $category)
+                                <option value="{{$category->id}}">{{$category->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <br>
+                    <input type="submit" value="作成"/>
+                </form>
+                <div class = "footer">
+                    <a href="/">戻る</a>
+                </div>
+            </body>
+        </x-slot>
+    </x-app-layout>
+</html>
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="{{str_replace('_','-', app()->getLocale())}}">
+
+    <head>
+        <meta charset="utf-8">
+        <title>GMcalendar</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200, 600" rel="stylesheet">
+    </head>
+
+    <x-app-layout>
+        <x-slot name="header">
+            <body>
+                <h1>グループ</h1>
+                <a href='/posts/create'>新規作成</a>
+                <div class='posts'>
+                    @foreach($posts as $post)
+                        <div class='post'>
+                            <h2 class='title'>
+                                <a href="/posts/{{$post->id}}">{{$post->title}}</a>
+                            </h2>
+                            <p class="category">
+                                <a href="/categories/{{$post->category->id}}">{{$post->category->name}}</a>
+                            </p>
+                            <p class='body'>{{$post->body}}</p>
+                    
+                            <form action="/posts/{{ $post->id }}" id="form_{{ $post->id}}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="button" onclick="deletePost({{$post->id}})">delete</button>
+                            </form>
+                            <br>
+                        </div>
+                    @endforeach
+                </div>
+                    <p>
+                        ログインユーザー:{{Auth::user()->name}}
+                    </p>
+
+                <script>
+                    function deletePost(id)
+                    {
+                        'use strict'
+                        
+                        if(confirm('削除すると復元できません。\n本当に削除しますか？')){
+                            document.getElementById(`form_${id}`).submit();
+                        }
+                    }
+                </script>
+            </body>
+        </x-slot>
+    </x-app-layout>
+        
+</html>
